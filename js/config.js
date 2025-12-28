0eeb1c767a00539b0a2e7f54bea6e79c045b5770
bvoms <81282549+bvoms@users.noreply.github.com>
2025-12-28 19:10:36 +0300

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -6,8 +6,13 @@
 export const SUPABASE_URL = 'https://xzzaoniuzvcxqfugvfzh.supabase.co';
 export const SUPABASE_KEY = 'sb_publishable_YAn708jttD0b0vAGLPreog_ZnCvFhAH';
 
-// ❗ ВАЖНО: createClient из SDK
+// ❗ SDK УЖЕ В window.supabase
+// ❗ ИМЕННО ТАК берём createClient
 const { createClient } = window.supabase;
+
+if (!createClient) {
+  throw new Error('Supabase SDK not loaded. Check script order in index.html');
+}
 
 export const supabase = createClient(
   SUPABASE_URL,
@@ -25,9 +30,3 @@ export const supabase = createClient(
 // TELEGRAM WEBAPP
 // ==========================
 export const tg = window.Telegram?.WebApp;
-
-// ==========================
-// TELEGRAM BOT (временно)
-// ==========================
-export const TG_BOT_TOKEN = '8321050426:AAH4fKadiex7i9NQnC7T2ZyjscRknQgFKlI';
-export const TG_CHAT_ID  = '-1003693227904';
